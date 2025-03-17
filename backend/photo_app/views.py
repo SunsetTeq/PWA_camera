24fd06e28cc42dc9df78851424729673683ca857
unknown <dotadgager@gmail.com>
2025-03-17 20:25:18 +0300

    bug fix

--- backend/photo_app/views.py
+++ backend/photo_app/views.py
@@ -1,4 +1,5 @@
 import os
+import time
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
@@ -11,8 +12,11 @@ def upload_photo(request):
     if request.method == 'POST' and request.FILES.get('photo'):
         photo = request.FILES['photo']
 
-        # Сохраняем файл в UPLOAD_FOLDER, можно добавить обработку или переименование
-        file_path = os.path.join(UPLOAD_FOLDER, photo.name)
+
+        name, extension = os.path.splitext(photo.name)
+        unique_filename = f"{name}_{int(time.time()*1000)}{extension}"  # добавляем метку времени для создания уникального имени
+
+        file_path = os.path.join(UPLOAD_FOLDER, unique_filename)
         with open(file_path, 'wb+') as destination:
             for chunk in photo.chunks():
                 destination.write(chunk)
